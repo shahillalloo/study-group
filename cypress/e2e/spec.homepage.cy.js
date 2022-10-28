6ff1b63a7cbba001f01c26cbc25582fa936b7dd7
Shahil Lalloo <lalloo@work.co>
2022-10-28 14:10:50 -0400

    Added homepage spec file with 4 test cases

---
+++ cypress/e2e/spec.homepage.cy.js
@@ -0,0 +1,32 @@
+describe('challenge-3 test cases', () => {
+
+  // test case 1 (check page title and headliner)
+  it('test1 opens w&c site and checks page title', () => {
+    cy.visit('https://work.co/')
+    cy.get('title').should('have.text', 'Work & Co | Digital Product Agency')
+    cy.get('[data-test-id="header-title-text"]').should('have.text', 'We design and ship digital products that transform companies.')
+  })
+
+  // test case 2 (click logo and check URL)
+  it('test2 opens w&c site and click logo', () => {
+    cy.visit('https://work.co/')
+    cy.get('[data-test-id="global-menu-btn"]').click()
+    cy.url().should('be.equal', 'https://work.co/grid/')
+  })
+
+  // test case 3 (click logo, click Select Clients and check URL)
+  it('test3 Select Clients', () => {
+    cy.visit('https://work.co/')
+    cy.get('[data-test-id="global-menu-btn"]').click()
+    cy.get('[data-test-id="grid-select-clients-link"]').click()
+    cy.url().should('be.equal', 'https://work.co/clients/')
+  })
+
+  // test case 4 (click logo, click 'Practice Areas', check URL)
+  it('test4 Practice Areas', () => {
+    cy.visit('https://work.co/')
+    cy.get('[data-test-id="global-menu-btn"]').click()
+    cy.get('[data-test-id="grid-practice-areas-link"]').click()
+    cy.url().should('be.equal', 'https://work.co/company/')
+  })
+})
