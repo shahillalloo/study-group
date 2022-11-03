4580ee3547117446a3a316fc7dec1c676ed2482f
shah <52937004+shahatwork@users.noreply.github.com>
2022-11-03 12:07:13 -0400

    Challenge 3 branch (#3)
    * challenge2
    
    * challenge2
    
    * Removes node_modules
    
    * Added instructions in README file and updated spec to include w&c header test
    
    * Fixed selector in spec file
    
    * Added homepage spec file with 4 test cases
    
    * deleted test files and updated cy title
    
    * fixed title with eq

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('w&c headline', () => {
-  it('opens w&c site and checks headline', () => {
-    cy.visit('https://work.co/')
-    cy.get('[data-test-id="header-title-text"]').should('have.text', 'We design and ship digital products that transform companies.')
-  })
-})
