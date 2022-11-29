42d39e3d758ead7c38c3a370bc6d86bd898e92de
Shahil Lalloo <lalloo@work.co>
2022-11-29 14:50:35 -0500

    Updated remaining instances of  class by using . prefix in cart file

--- cypress/e2e/spec.cart.cy.js
+++ cypress/e2e/spec.cart.cy.js
@@ -5,7 +5,7 @@ describe('sauce demo cart flows', () => {
     //test1 error with no data
     it('try to checkout without any items added to cart', () => {
         // click cart
-        cy.get('[class="shopping_cart_link"]').click()
+        cy.get('.shopping_cart_link').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/cart.html')
         // click checkout
         cy.get('[data-test="checkout"]').click()
@@ -19,19 +19,19 @@ describe('sauce demo cart flows', () => {
     //test2 successful shopping flow
     it('add item to cart and successfully checkout', () => {
         // validate cart badge doesn't exist
-        cy.get('[class="shopping_cart_badge"]').should('not.exist')
+        cy.get('.shopping_cart_badge').should('not.exist')
         // add backpack to cart from inventory page
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         // validate cart badge displays 1
-        cy.get('[class="shopping_cart_badge"]').should('exist')
-        cy.get('[class="shopping_cart_badge"]').should('contain', '1')
+        cy.get('.shopping_cart_badge').should('exist')
+        cy.get('.shopping_cart_badge').should('contain', '1')
         // go to cart
-        cy.get('[class="shopping_cart_link"]').click()
+        cy.get('.shopping_cart_link').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/cart.html')
         // validate item is in cart
-        cy.get('[class="inventory_item_name"]').should('have.text', 'Sauce Labs Backpack')
-        cy.get('[class="cart_quantity"]').should('contain', '1')
-        cy.get('[class="inventory_item_price"]').should('have.text', '$29.99')
+        cy.get('.inventory_item_name').should('have.text', 'Sauce Labs Backpack')
+        cy.get('.cart_quantity').should('contain', '1')
+        cy.get('.inventory_item_price').should('have.text', '$29.99')
         // validate remove, continue, checkout buttons display
         cy.get('[data-test="remove-sauce-labs-backpack"]').should('exist')
         cy.get('[data-test="continue-shopping"]').should('exist')
@@ -50,19 +50,19 @@ describe('sauce demo cart flows', () => {
         cy.get('[data-test="continue"]').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/checkout-step-two.html')
         // validate item is in order
-        cy.get('[class="inventory_item_name"]').should('have.text', 'Sauce Labs Backpack')
-        cy.get('[class="cart_quantity"]').should('contain', '1')
-        cy.get('[class="inventory_item_price"]').should('have.text', '$29.99')
+        cy.get('.inventory_item_name').should('have.text', 'Sauce Labs Backpack')
+        cy.get('.cart_quantity').should('contain', '1')
+        cy.get('.inventory_item_price').should('have.text', '$29.99')
         // click finish
         cy.get('[data-test="finish"]').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/checkout-complete.html')
         // validate confirmation page (header and back home button)
-        cy.get('[class="complete-header"]').should('contain', 'THANK YOU FOR YOUR ORDER')
+        cy.get('.complete-header').should('contain', 'THANK YOU FOR YOUR ORDER')
         cy.get('[data-test="back-to-products"]').should('exist')
         // click back home button, validate you return to inventory page and cart is empty
         cy.get('[data-test="back-to-products"]').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/inventory.html')
-        cy.get('[class="shopping_cart_badge"]').should('not.exist')
+        cy.get('shopping_cart_badge').should('not.exist')
     })
 
     //test3 remove item
@@ -73,7 +73,7 @@ describe('sauce demo cart flows', () => {
         cy.get('.shopping_cart_badge').should('exist')
         cy.get('.shopping_cart_badge').should('contain', '1')
         // navigate to cart and remove item
-        cy.get('[class="shopping_cart_link"]').click()
+        cy.get('.shopping_cart_link').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/cart.html')
         cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]').click()
         // validate bag is empty
@@ -85,14 +85,14 @@ describe('sauce demo cart flows', () => {
         // add bike light to cart from inventory page
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
         // navigate to cart 
-        cy.get('[class="shopping_cart_link"]').click()
+        cy.get('.shopping_cart_link').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/cart.html')
         // click continue shopping button to return to inventory page
         cy.get('[data-test="continue-shopping"]').click()
         cy.url().should('be.equal', 'https://www.saucedemo.com/inventory.html')
         // validate cart badge displays 1
-        cy.get('[class="shopping_cart_badge"]').should('exist')
-        cy.get('[class="shopping_cart_badge"]').should('contain', '1')
+        cy.get('.shopping_cart_badge').should('exist')
+        cy.get('.shopping_cart_badge').should('contain', '1')
     })
 })
 
