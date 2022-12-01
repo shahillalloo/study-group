cc021036b68e70b10476fa38a7c85c699cf50655
Shahil Lalloo <lalloo@work.co>
2022-12-01 10:34:59 -0500

    Commit to include changes on command file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,7 +26,7 @@
 
 Cypress.Commands.add('login', (username, password) => {
   cy.visit('https://www.saucedemo.com/')
-  cy.get('[data-test="username"]').type("standard_user")
-  cy.get('[data-test="password"]').type("secret_sauce")
+  cy.get('[data-test="username"]').type("username")
+  cy.get('[data-test="password"]').type("password")
   cy.get('[data-test="login-button"]').click()
 })
